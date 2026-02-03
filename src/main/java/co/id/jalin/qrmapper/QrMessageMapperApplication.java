ba83e012078cdd2a2e496066a8c16206340aa3a9
Jaka Brajadenta <79687223+jakabrajadenta@users.noreply.github.com>
2026-02-03 10:36:57 +0700

    Initial project

---
+++ src/main/java/co/id/jalin/qrmapper/QrMessageMapperApplication.java
@@ -0,0 +1,13 @@
+package co.id.jalin.qrmapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrMessageMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrMessageMapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/id/jalin/qrmapper/QrMessageMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.jalin.qrmapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrMessageMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
